b8d8c022894995981c035ee9b6134fb82423da77
aanubhavv <anubhavg60@gmail.com>
2025-10-11 21:52:39 +0530

    Refactored List component to accept items and category as props for dynamic rendering

--- src/List.jsx
+++ src/List.jsx
@@ -1,23 +1,18 @@
-function List() {
-  const fruits = [{id: 1, name: "apple", calories: 98}, 
-                  {id: 2, name: "orange", calories: 45}, 
-                  {id: 3, name: "banana", calories: 105}, 
-                  {id: 4, name: "coconut", calories: 159}, 
-                  {id: 5, name: "pineapple", calories: 37}]
-  
-  // fruits.sort((a, b) => a.name.localeCompare(b.name)) //Alphabetical order
-  // fruits.sort((a, b) => b.name.localeCompare(a.name)) //Reverse Alphabetical order
-  // fruits.sort((a, b) => a.calories - b.calories) //Numerically
-  // fruits.sort((a, b) => b.calories - a.calories) //Reverse Numerically
+function List(props) {
 
-  const lowCalFruits = fruits.filter(fruit => fruit.calories < 100)
-  const highCalFruits = fruits.filter(fruit => fruit.calories >= 100)
+  const catagory = props.catagory
+  const itemList = props.items
 
-  const listItems = highCalFruits.map(highCalFruit => <li key={highCalFruit.id}>
-                                                        {highCalFruit.name}: &nbsp;
-                                                        <b>{highCalFruit.calories}</b></li>)
+  const listItems = itemList.map(item => <li key={item.id}>
+                                        {item.name}: &nbsp;
+                                        <b>{item.calories}</b></li>)
 
-  return(<ol>{listItems}</ol>)
+  return(
+    <>
+      <h3>{catagory}</h3>
+      <ol>{listItems}</ol>
+    </>
+  )
 }
 
 export default List
